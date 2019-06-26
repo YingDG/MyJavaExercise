16636be43a99d20c9f92f2786e5ea2417e9154d9
YingDG <zdm.1991@outlook.com>
2019-06-26 11:18:29 +0800

    java.net socket update

--- src/yingdg/exercise/se/net/NetMain.java
+++ src/yingdg/exercise/se/net/NetMain.java
@@ -1,7 +1,24 @@
 package yingdg.exercise.se.net;
 
+import sun.security.util.SecurityConstants;
+
+import java.net.CookieHandler;
+import java.net.FileNameMap;
+import java.net.HttpURLConnection;
+import java.net.IDN;
 import java.net.MalformedURLException;
+import java.net.PasswordAuthentication;
+import java.net.ProxySelector;
+import java.net.SocketOptions;
+import java.net.StandardProtocolFamily;
+import java.net.StandardSocketOptions;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.Arrays;
 
 /**
  * @author YingDG
@@ -24,9 +41,57 @@ public class NetMain {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+        System.out.println(URLEncoder.encode("hello"));
+        System.out.println(URLDecoder.decode("hello"));
 
         /*
         ServerSocket类，Socket类
+        SocketAddress, InetSocketAddress,
          */
+
+        /*
+        SocketOptions
+         */
+        System.out.println(SocketOptions.SO_KEEPALIVE);
+
+        // BindException, ContentHandlerFactory, CookieManager, CookiePolicy, FileNameMap,
+        // HttpCookie, InetAddress, Inet4Address, Inet6Address,
+        // SocketAddress, InetSocketAddress, MulticastSocket, NetPermission,
+        // Proxy, SocketOption, SocketPermissionCollection,
+
+        CookieHandler cookieHandler = CookieHandler.getDefault();
+
+        System.out.println(SecurityConstants.SOCKET_ACCEPT_ACTION);
+
+        /*
+        URLConnection, HttpURLConnection
+         */
+        System.out.println(HttpURLConnection.HTTP_OK);
+        System.out.println(HttpURLConnection.HTTP_NOT_FOUND);
+
+        System.out.println(IDN.toASCII("hello"));
+        System.out.println(IDN.toUnicode("hello"));
+
+        System.out.println(new PasswordAuthentication("yingdg", "password".toCharArray()));
+
+        System.out.println(Arrays.toString(StandardProtocolFamily.values()));
+
+        ProxySelector proxySelector = ProxySelector.getDefault();
+
+        System.out.println(StandardSocketOptions.SO_KEEPALIVE);
+
+        FileNameMap fileNameMap = URLConnection.getFileNameMap();
+
+        try {
+            URI uri = new URI("http://www.runoob.com/html/html-tutorial.html#");
+            System.out.println(uri.getHost());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        /*
+        URLPermission
+         */
+
     }
 }
